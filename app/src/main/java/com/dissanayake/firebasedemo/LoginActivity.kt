8511fb0d0467e3714cabab7ea5b3ee6e9133fd6a
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-01-12 02:07:17 +0530

    updated

---
+++ app/src/main/java/com/dissanayake/firebasedemo/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.dissanayake.firebasedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/dissanayake/firebasedemo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dissanayake.firebasedemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val navToRegisterActivity = Intent(this, RegisterActivity::class.java)
+            startActivity(navToRegisterActivity)
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            val navToLoginActivity = Intent(this, LoginActivity::class.java)
+            startActivity(navToLoginActivity)
+        }
+
+    }
+}
