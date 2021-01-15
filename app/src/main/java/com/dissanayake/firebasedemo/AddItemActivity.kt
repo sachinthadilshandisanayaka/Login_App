57f094ed3587c3d1b41435c8c3eac5314bc7dde0
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2021-01-15 06:31:01 +0530

    updated

--- app/src/main/java/com/dissanayake/firebasedemo/AddItemActivity.kt
+++ app/src/main/java/com/dissanayake/firebasedemo/AddItemActivity.kt
@@ -1,20 +1,56 @@
 package com.dissanayake.firebasedemo
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dissanayake.firebasedemo.fragment.ListItemFragment
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+import com.google.firebase.database.*
+import java.lang.NullPointerException
 
 class AddItemActivity : AppCompatActivity() {
+
+    private lateinit var refUsers: DatabaseReference
+    private lateinit var listItemActivity: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
+        try {
+            listItemActivity = findViewById<ListView>(R.id.list_items) //
+        } catch (e: NullPointerException) {
+            Log.i("Error", e.toString())
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
 
-        val listItemFragment = ListItemFragment()
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frameLayout, listItemFragment)
-            addToBackStack(null)
-            commit()
-        }
+        val listItem = ArrayList<String>()
+        val adapter:ArrayAdapter<String> = ArrayAdapter(this, R.layout.list_view, listItem)
+        listItemActivity.adapter = adapter
+
+        refUsers = FirebaseDatabase.getInstance("https://androidapptest-8b7ac-default-rtdb.firebaseio.com/").reference.child("products")
+        refUsers.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot){
+                listItem.clear()
+                for (snapshot:DataSnapshot in dataSnapshot.children) {
+                    listItem.add(snapshot.value.toString())
+                }
+                adapter.notifyDataSetChanged()
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("Error :", "Failed to read value.", error.toException())
+            }
+        })
+
     }
 }
+
+
+
+
+
+
+
+
